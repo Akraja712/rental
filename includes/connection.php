ce42b744476947d0f85d3b7fc71acd4a85ee1326
Akraja712 <ashokkumar7hitter@gmail.com>
2024-09-24 10:17:27 +0530

    crud.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,4 @@
 <?php
-define('DOMAIN_URL', 'https://moneybook.site/');
-define('API_URL', 'https://moneybook.site/admin_v1/api/');
+define('DOMAIN_URL', 'http://rental.graymatterworks.com/');
+define('API_URL', 'http://rental.graymatterworks.com/admin_v1/api/');
 ?>
